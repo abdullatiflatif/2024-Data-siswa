53d1ffd3076f78f36ffe0a3dae094ce98ed2f95f
alatiff045@gmail.com <alatiff045@gmail.com>
2024-01-22 11:14:04 +0700

    menambah konfigurasi fayarbese

---
+++ main.js
@@ -0,0 +1,26 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { 
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deletDoc,
+  doC,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdgAITXTldockRB_wgxOrbNUPKzSVBhDs",
+  authDomain: "insan-cemerlang-c9554.firebaseapp.com",
+  projectId: "insan-cemerlang-c9554",
+  storageBucket: "insan-cemerlang-c9554.appspot.com",
+  messagingSenderId: "753628555075",
+  appId: "1:753628555075:web:7a72b2d1e8ae89716931f6",
+  measurementId: "G-KMJZ5V0B8H"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
